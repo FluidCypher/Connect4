a82a233a265f64cbbee634eaf871cb6de227ed73
kaiwalya-ops <67866250+kaiwalya-ops@users.noreply.github.com>
2021-09-30 20:05:29 +0530

    Added victory Logic

--- connect.py
+++ connect.py
@@ -1,36 +1,76 @@
+from numpy.typing import _128Bit
 import pygame
-import numpy
 def construct(x,y):
     global board 
-    board = numpy.zeros((x,y))
+    board = [[0 for i in range(y)] for j in range(x)]
 
 def level():
     i=int(input("Enter 1 2 3 for increasing size : "))
     if(i==1):
-        (row,column)=(4,5)
-    elif(i==2):
         (row,column)=(6,7)
-    else:
+    elif(i==2):
         (row,column)=(8,9)
+    else:
+        (row,column)=(10,11)
     construct(row,column)
     return((row,column))
 
+def display():
+    for i in board:
+        for j in i:
+            print(j,end='  ')
+        print()
+
 def check_legal(y):
-    if(board[0,y]==0):
+    if(board[0][y-1]==0):
         return(True)
     else:
+        print("Overflow")
         return(False)
 
-def insert(column,x):
-    for i in range(column):
-        if (board[i,column]!=0):
-            board[i-1,column]=x
+def insert(position,player):
+    for i in range(row-1,-1,-1):
+        if board[i][position]==0.0:
+            board[i][position]=player+1
+            break
+    return((i,position))
 
-def victory():
-    return(True)
+def horizontal(player,posi):
+    s=''
+    for i in board[posi[0]]:
+        s+=str(i)
+    print(s,str(player)*4)
+    if str(player)*4 in s:
+        return(True)
+    return(False)
+
+def vertical(player,posi):
+    s=''
+    for i in board:
+        s+=str(i[posi[1]])
+    print(s)
+    if str(player)*4 in s:
+        return(True)
+    return(False)
+
+def victory(player,posi):
+    if posi==tuple():
+        return(False)
+    return(horizontal(3-player,posi) or vertical(3-player,posi))# or rdiagonal() or ldiagonal())
+
+def play(player):
+    posi=tuple()
+    while(not victory(player+1,posi)):
+        print("Player : ",player+1)
+        position=int(input("Enter Positon : "))
+        if not check_legal(position):
+            play(player)
+
+        posi=insert(position-1,player)
+        player=(player+1)%2  
+        display()
+
 
 (row,column)=level()
-
-player=0
-while(victory()):
-    continue
+player=1
+play(player)
